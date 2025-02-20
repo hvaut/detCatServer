45111bfc80534a2acfa7df757a8b152184234f50
Lumin007 <simillion09@gmail.com>
2025-02-20 08:29:57 +0100

    test

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@
 /**
  * Beschreiben Sie hier die Klasse Test.
  * 
- * @author (Ihr Name) 
+ * @author (Tim Göllner) 
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Test
@@ -34,17 +34,40 @@ public class Test
         x = 0;
 
         //Frederick
+<<<<<<< HEAD
+=======
 
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
         //Lumin007
+<<<<<<< HEAD
+       
+=======
 
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
         //Hannes
         // tim
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
 
         //David
 
 
+<<<<<<< HEAD
 
+=======
+=======
+>>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
+        x = 0;
+>>>>>>> dcf669b98be608183e4c4939ea51834166e409de
+>>>>>>> 9a434f02c2a2f3127410b56b585714e1743653ec
+>>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
     }
 
     /**
