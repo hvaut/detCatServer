4b10b89ce26429c2136907b6b583344fe430ca1d
TheSnapped <jannes2507074@gmail.com>
2025-02-20 08:30:39 +0100

    name klasse

--- Test.java
+++ Test.java
@@ -16,51 +16,20 @@ public class Test
     public Test()
     {
         //Gucas
-<<<<<<< HEAD
-
-<<<<<<< HEAD
         //Lion
-
-=======
         //Lumin007   
-
-=======
-<<<<<<< HEAD
-
         //Lion
 
         // Instanzvariable initialisieren
         // tim
-
-        x = 0;
-=======
->>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
         // Instanzvariable initialisieren
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        x = 0;
->>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
         //Frederick
 
         //Lumin007
 
         //Hannes
->>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
         // tim
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
         //David
-
-
-=======
->>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
->>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
-        x = 0;
-=======
         // Instanzvariable initialisieren
         //Jannes
         // tim
@@ -68,10 +37,6 @@ public class Test
         //Lumin007 
         //Hannes
         // tim
-        x = 0;
-<<<<<<< HEAD
-=======
->>>>>>> dcf669b98be608183e4c4939ea51834166e409de
 }
 
     /**
