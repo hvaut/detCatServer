c2a8aac9dd16a41dea1426e57130d05b1001e7a6
ExplodingHinz <l.adler@lau365.de>
2025-02-20 08:41:31 +0100

    gb

---
+++ Test.java
@@ -0,0 +1,43 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Test.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Test
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Test
+     */
+    public Test()
+    {
+<<<<<<< HEAD
+        // kekospielt
+=======
+        // Instanzvariable initialisieren
+<<<<<<< HEAD
+        
+        //Hannes
+=======
+        // tim
+>>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
+        x = 0;
+>>>>>>> e61783fce76aa597884b867f5a8f2a9feb44d3c4
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
