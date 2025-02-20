25f91013f1871432811b96794d2f861b85c47149
Vincenz <v.vu@lau365.de>
2025-02-20 08:30:53 +0100

    .

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@
 /**
  * Beschreiben Sie hier die Klasse Test.
  * 
+<<<<<<< HEAD
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
@@ -22,6 +23,67 @@ public class Test
         //Hannes
         // tim
         x = 0;
+=======
+ * @author (Tim Göllner) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Test
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Test
+     */
+    public Test()
+    {
+        //Gucas
+<<<<<<< HEAD
+
+        //Lumin007   
+
+=======
+<<<<<<< HEAD
+
+        //Lion
+
+        // Instanzvariable initialisieren
+        // tim
+
+        x = 0;
+=======
+        // Instanzvariable initialisieren
+
+        x = 0;
+>>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
+        //Frederick
+<<<<<<< HEAD
+=======
+
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+        //Lumin007
+<<<<<<< HEAD
+       
+=======
+
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+        //Hannes
+        // tim
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+        //David
+
+
+=======
+>>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
+        x = 0;
+>>>>>>> dcf669b98be608183e4c4939ea51834166e409de
+>>>>>>> 9a434f02c2a2f3127410b56b585714e1743653ec
+>>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
     }
 
     /**
