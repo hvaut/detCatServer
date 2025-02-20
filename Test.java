afcf01b432f93624318db6a0fb07242afe409dba
Vincenz <v.vu@lau365.de>
2025-02-20 08:33:12 +0100

    dfsdf

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@
  * Beschreiben Sie hier die Klasse Test.
  * 
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
@@ -84,6 +85,134 @@ public class Test
 >>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
 >>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
 >>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+=======
+ * @author () 
+=======
+ * @author (Gucas Lörzen) 
+>>>>>>> 78c9f0576c1ee0666586dc66cab19d559dcd7020
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Test
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Test
+     */
+    public Test()
+    {
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        //Gucas
+=======
+>>>>>>> d361d7d4d0094082bd9095fdc2f614d0196a77f5
+<<<<<<< HEAD
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+
+
+
+        // Instanzvariable initialisieren
+        // Purrrrrrrminator 
+ 
+
+        //Gucas
+
+=======
+        //Gucas
+<<<<<<< HEAD
+>>>>>>> 7988b0ac9107831c377f75e92c2714bbbd03a7e4
+
+        //Lumin007   
+
+        //Lion
+
+        // Instanzvariable initialisieren
+        // tim
+
+<<<<<<< HEAD
+        x = 0;
+=======
+>>>>>>> d361d7d4d0094082bd9095fdc2f614d0196a77f5
+        // Instanzvariable initialisieren
+<<<<<<< HEAD
+=======
+
+<<<<<<< HEAD
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+=======
+<<<<<<< HEAD
+
+=======
+>>>>>>> d361d7d4d0094082bd9095fdc2f614d0196a77f5
+        x = 0;
+>>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
+>>>>>>> 7988b0ac9107831c377f75e92c2714bbbd03a7e4
+        //Frederick
+<<<<<<< HEAD
+=======
+
+<<<<<<< HEAD
+>>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
+=======
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+        //Lumin007
+<<<<<<< HEAD
+<<<<<<< HEAD
+        
+=======
+
+>>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
+=======
+       
+=======
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+<<<<<<< HEAD
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+=======
+>>>>>>> 7988b0ac9107831c377f75e92c2714bbbd03a7e4
+>>>>>>> d361d7d4d0094082bd9095fdc2f614d0196a77f5
+        //Hannes
+
+
+        // tim
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 7988b0ac9107831c377f75e92c2714bbbd03a7e4
+
+        //David
+
+
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
+>>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
+        x = 0;
+<<<<<<< HEAD
+=======
+>>>>>>> dcf669b98be608183e4c4939ea51834166e409de
+>>>>>>> 9a434f02c2a2f3127410b56b585714e1743653ec
+>>>>>>> 6017d7189b486ea743bca6d1061e3ed6355e3329
+<<<<<<< HEAD
+>>>>>>> 4546447a0a29defd1efe0b960f0df60f5e01b5d2
+=======
+>>>>>>> 830bb7d42163c9edd0d9b5290d1beb6fe82b7e99
+<<<<<<< HEAD
+>>>>>>> b9e4c117b3e0ede1b9159b9e8884ab0e82eb300b
+=======
+>>>>>>> 7988b0ac9107831c377f75e92c2714bbbd03a7e4
+>>>>>>> d361d7d4d0094082bd9095fdc2f614d0196a77f5
+>>>>>>> 26e676848fb9c2dbb73f47f46646f5383d6fd27a
     }
 
     /**
