38a9d2e76caff025fbe751d28292b85454f8e5b5
Fr3der1x <janzenfrederick@gmail.con>
2025-02-20 08:26:50 +0100

    Name eingetragen

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@
 /**
  * Beschreiben Sie hier die Klasse Test.
  * 
- * @author (Ihr Name) 
+ * @author (Tim Göllner) 
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Test
@@ -16,22 +16,13 @@ public class Test
     public Test()
     {
         // Instanzvariable initialisieren
-<<<<<<< HEAD
+
         x = 0;
         //Frederick
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
         //Lumin007
-=======
-        
+       
         //Hannes
->>>>>>> b8367786fb9d522a6b54fe370513f7477b4e8717
-=======
         // tim
->>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
-        x = 0;
->>>>>>> dcf669b98be608183e4c4939ea51834166e409de
     }
 
     /**
