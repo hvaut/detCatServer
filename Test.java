f81a3f8d35fae9855e683914d4359685f8cb4103
KiliWien <kiliwien@gmail.com>
2025-02-20 08:29:34 +0100

    f

--- Test.java
+++ Test.java
@@ -17,16 +17,28 @@ public class Test
     {
         // Instanzvariable initialisieren
 <<<<<<< HEAD
+<<<<<<< HEAD
         //Kili
 =======
 <<<<<<< HEAD
+=======
+        x = 0;
+        //Frederick
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+        //Lumin007
+=======
+>>>>>>> 9a434f02c2a2f3127410b56b585714e1743653ec
         
         //Hannes
+>>>>>>> b8367786fb9d522a6b54fe370513f7477b4e8717
 =======
         // tim
 >>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
 >>>>>>> b8367786fb9d522a6b54fe370513f7477b4e8717
         x = 0;
+>>>>>>> dcf669b98be608183e4c4939ea51834166e409de
     }
 
     /**
