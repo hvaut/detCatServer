9020850c916d2164b6e5412f94b624fee1c4f6e7
sirroti <sirroti.test@gmail.com>
2025-02-20 08:25:03 +0100

    recover

--- Test.java
+++ Test.java
@@ -15,7 +15,6 @@ public class Test
      */
     public Test()
     {
-<<<<<<< HEAD
 
         //Lion
 
@@ -23,25 +22,14 @@ public class Test
         // tim
 
         x = 0;
-=======
         // Instanzvariable initialisieren
-<<<<<<< HEAD
         x = 0;
         //Frederick
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
         //Lumin007
-=======
         
         //Hannes
->>>>>>> b8367786fb9d522a6b54fe370513f7477b4e8717
-=======
         // tim
->>>>>>> 1dbf33a43b2b94681034b76cb909480156a18827
         x = 0;
->>>>>>> dcf669b98be608183e4c4939ea51834166e409de
->>>>>>> 9a434f02c2a2f3127410b56b585714e1743653ec
     }
 
     /**
