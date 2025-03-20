17b03fc6ea395673fa3cb16c388df1771aed7b98
LurkingBytes <183619118+LurkingBytes@users.noreply.github.com>
2025-03-20 17:11:42 +0100

    Impl basic server methods

--- DCServer.java
+++ DCServer.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class DCServer extends Server {
 
     private List<Player> players;
@@ -7,42 +5,85 @@ public class DCServer extends Server {
 
     public DCServer(int port) {
         super(port);
+
+        players = new List<>();
+        games = new List<>();
     }
 
     public void processNewConnection(String ip, int port) {
     }
 
     public void processClosingConnection(String ip, int port) {
+        Player player = getPlayer(ip, port);
+
+        if (player != null) {
+            removePlayer(player);
+        }
     }
 
     public void processMessage(String ip, int port, String message) {
+        String[] data = message.split(" ");
+
+        // TODO
     }
 
     public void addGame(Game game) {
+        games.append(game);
     }
 
     public void removeGame(Game game) {
+        games.toFirst();
+        while (games.hasAccess()) {
+            if (games.getContent().getId() == game.getId())
+                games.remove();
+            games.next();
+        }
     }
 
     public Game getGame(int id) {
+        games.toFirst();
+        while (games.hasAccess()) {
+            if (games.getContent().getId() == id)
+                return games.getContent();
+            games.next();
+        }
         return null;
     }
 
     public Game getGame2(Player player) {
-        return null;
+        return player.getGame();
     }
 
     public void addPlayer(Player player) {
+        players.append(player);
     }
 
     public void removePlayer(Player player) {
+        players.toFirst();
+        while (players.hasAccess()) {
+            if (players.getContent().getIp() == player.getIp() && players.getContent().getPort() == player.getPort())
+                players.remove();
+            players.next();
+        }
     }
 
     public Player getPlayer(String ip, int port) {
+        players.toFirst();
+        while (players.hasAccess()) {
+            if (players.getContent().getIp().equalsIgnoreCase(ip) && players.getContent().getPort() == port)
+                return players.getContent();
+            players.next();
+        }
         return null;
     }
 
     public Player getPlayer2(String name) {
+        players.toFirst();
+        while (players.hasAccess()) {
+            if (players.getContent().getName().equalsIgnoreCase(name))
+                return players.getContent();
+            players.next();
+        }
         return null;
     }
 
